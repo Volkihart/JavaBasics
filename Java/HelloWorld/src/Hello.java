c9ec989d6f541d1f6c83b23dea14b9c0de7bd67a
VolkiHart <welslyhartman@gmail.com>
2021-05-31 16:25:13 -0500

    Update Hello.java

--- Java/HelloWorld/src/Hello.java
+++ Java/HelloWorld/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wes");
+        System.out.println("Hello World");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;

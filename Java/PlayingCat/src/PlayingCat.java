2ca1580c087fb849837728a598f73c7d8efde5d2
VolkiHart <welslyhartman@gmail.com>
2021-06-12 18:23:55 -0500

    Add files via upload

---
+++ Java/PlayingCat/src/PlayingCat.java
@@ -0,0 +1,14 @@
+public class PlayingCat {
+
+    public static boolean isCatPlaying (boolean summer, int temperature){
+        if((temperature > 24) && (temperature < 36)){
+            System.out.println("true");
+            return true;
+        } else if((summer) && (temperature > 24) && (temperature < 46)){
+            System.out.println("true");
+            return true;
+        } else
+            System.out.println("false");
+        return false;
+    }
+}
